55205d90b3043eb4aac2307d29aab290e53a4fd6
caltistals <y.db.qp.y@gmail.com>
2023-02-08 02:08:16 +0900

    add: Headerのstoriesを作成

---
+++ src/components/header/Header.stories.tsx
@@ -0,0 +1,8 @@
+import { ComponentMeta } from "@storybook/react";
+import Header from "./Header";
+
+export default {
+  component: Header,
+} as ComponentMeta<typeof Header>;
+
+export const Default = {};
