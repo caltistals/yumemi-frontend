6792db3a1e57929e3c89f0cd606ba74ae2c2324e
caltistals <y.db.qp.y@gmail.com>
2023-02-09 23:27:50 +0900

    add: ReactQueryDevtoolsを追加

--- src/providers/AppProvider.tsx
+++ src/providers/AppProvider.tsx
@@ -1,4 +1,5 @@
 import { QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import React from "react";
 import { queryClient } from "../lib/react-query";
 
@@ -8,6 +9,10 @@ type Props = {
 
 export const AppProvider = ({ children }: Props) => {
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
