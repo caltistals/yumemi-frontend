ee36afde7cfe37d3a764da971006de3356c535bb
caltistals <y.db.qp.y@gmail.com>
2023-02-10 02:04:55 +0900

    update: PageにPopulationGraphを配置

--- src/features/prefecture-population/components/index.tsx
+++ src/features/prefecture-population/components/index.tsx
@@ -1,9 +1,12 @@
 import Header from "../../../components/header/Header";
+import { usePrefecturePopulations } from "../hooks/usePrefecturePopulations";
 import { usePrefectureCheckboxes } from "../hooks/usePrefectureCheckboxes";
+import PopulationGraph from "./population-graph/PopulationGraph";
 import PrefectureCheckboxes from "./prefecture-checkboxes/PrefectureCheckboxes";
 
 export const PrefecturePopulationPage = () => {
   const { prefectureCheckboxes, toggle } = usePrefectureCheckboxes();
+  const { populationData } = usePrefecturePopulations(prefectureCheckboxes);
 
   return (
     <>
@@ -12,6 +15,7 @@ export const PrefecturePopulationPage = () => {
         prefectureCheckboxes={prefectureCheckboxes}
         toggle={toggle}
       />
+      <PopulationGraph populationData={populationData()} />
     </>
   );
 };
