5a42a90b96582c295eb011113d1d264158d82cb1
caltistals <y.db.qp.y@gmail.com>
2023-02-09 23:28:51 +0900

    update: resasClient生成時にヘッダーを指定するように変更

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -3,4 +3,7 @@ import axios from "axios";
 export const resasClient = axios.create({
   baseURL: "https://opendata.resas-portal.go.jp/api/v1/",
   timeout: 5000,
+  headers: {
+    "X-API-KEY": import.meta.env.VITE_RESAS_API_KEY,
+  },
 });
